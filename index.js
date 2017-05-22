bdf78317c05d1ab921672eff5d9b0371fa26626c
root <root@localhost.localdomain>
2017-05-22 13:33:47 +0900

    supported delete type

--- index.js
+++ index.js
@@ -61,11 +61,53 @@ mofron.event.Drag = class extends mofron.Event {
             if (undefined === this.m_type) {
                 this.m_type = {};
             }
-            this.m_type[type] = null;
+            this.m_type[type] = true;
             if (null === this.target()) {
                 return;
             }
             this.setType(type);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    delType (tp) {
+        try {
+            if ('string' !== typeof tp) {
+                if ( ('object' === typeof tp) && (undefined !== tp[0]) ) {
+                    for (let tp_idx in tp) {
+                        this.delType(tp[tp_idx]);
+                    }
+                    return;
+                }
+                throw new Error('invalid parameter');
+            }
+            if (undefined === this.m_type) {
+                this.m_type = {};
+            }
+            if (undefined === this.m_type[tp]) {
+                return;
+            }
+            this.m_type[tp] = false;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    getType () {
+        try {
+            if (undefined === this.m_type) {
+                return null;
+            }
+            let ret_val = new Array();
+            for (let tp_idx in this.m_type) {
+                if (true === this.m_type[tp_idx]) {
+                    ret_val.push(tp_idx);
+                }
+            }
+            return (0 === ret_val.length) ? null : ret_val;
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -80,9 +122,17 @@ mofron.event.Drag = class extends mofron.Event {
             let evt  = this;
             let fnc  = () => {
                 try {
-                    let evt_cb = evt.handler();
-                    if (null != evt_cb[0]) {
-                        evt_cb[0](evt.target(), tp, evt_cb[1]);
+                    let evt_type = evt.getType();
+                    if (null === evt_type) {
+                        return;
+                    }
+                    let evt_cb = evt.handler();;
+                    for (let tp_idx in evt_type) {
+                        if (tp === evt_type[tp_idx]) {
+                            if (null != evt_cb[0]) {
+                                evt_cb[0](evt.target(), tp, evt_cb[1]);
+                            }
+                        }
                     }
                 } catch (e) {
                     console.error(e.stack);
